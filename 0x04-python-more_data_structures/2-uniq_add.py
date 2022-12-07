af6277a7ce501477350405945cbb9563be0f682d
bami98 <beamlaksamson4@gmail.com>
2022-12-07 08:24:36 -0800

    adds all unique integers in a list (only once for each integer)

---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+from functools import reduce
+def uniq_add(my_list=[]):
+    """ adds all unique integers in a list (only once for each integer)
+
+    Keyword arguments:
+    argument -- list
+    Return: sum of the list
+    """
+    uniq_list = list(set(my_list))
+    sum = reduce((lambda x, y: x + y), uniq_list)
+    return sum
